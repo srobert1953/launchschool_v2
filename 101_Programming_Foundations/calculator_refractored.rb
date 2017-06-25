4b57c4c1a97d7a1b550b685d5822a955138ef29f
srobert1953 <srobert1953@gmail.com>
2017-06-25 21:41:04 +0200

    Calculator refractored

--- 101_Programming_Foundations/calculator_refractored.rb
+++ 101_Programming_Foundations/calculator_refractored.rb
@@ -1,33 +1,26 @@
 # calculator_refractored.rb
 
-def prompt str
+def prompt(str)
   puts "=> #{str}"
 end
 
-def valid_number? number
+def valid_number?(number)
   number == number.to_i.to_s
 end
 
-def get_number turn
+def get_number(turn)
   number = ''
   loop do
     prompt "Please enter the #{turn} number:"
     number = gets.chomp
 
-    if valid_number? number
-      break
-    else
-      prompt "This doesn't look like an integer..."
-    end
+    break if valid_number? number
+    prompt "This doesn't look like an integer..."
   end
   number
 end
 
-loop do # main loop
-  number1 = get_number "first"
-
-  number2 = get_number "second"
-
+def operand
   operator_prompt = <<-MSG
     Please choose an operation:
     1) add,
@@ -35,17 +28,23 @@ loop do # main loop
     3) multiply,
     4) divide
   MSG
-
   prompt operator_prompt
+
   operation = ''
   loop do
     operation = gets.chomp
-    if %w(1 2 3 4 ).include? operation
-      break
-    else
-      prompt "Please choose 1, 2, 3, or 4."
-    end
+    break if %w(1 2 3 4).include? operation
+    prompt "Please choose 1, 2, 3, or 4."
   end
+  operation
+end
+
+loop do # main loop
+  number1 = get_number "first"
+
+  number2 = get_number "second"
+
+  operation = operand
 
   result = case operation
            when '1' then number1.to_i + number2.to_i
