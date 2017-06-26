eb369abf588ea498561379c1135500cd249e5748
srobert1953 <srobert1953@gmail.com>
2017-06-26 18:37:30 +0200

    Calculator refractored

--- 101_Programming_Foundations/calculator_refractored.rb
+++ 101_Programming_Foundations/calculator_refractored.rb
@@ -1,48 +1,56 @@
 # calculator_refractored.rb
 
+require 'yaml'
+
+MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'en'
+
 def prompt(str)
   puts "=> #{str}"
 end
 
+def integer?(number)
+  /^\d+$/.match(number)
+end
+
+def float?(number)
+  /\d/.match(number) && /^\d*\.?\d*$/.match(number)
+end
+
 def valid_number?(number)
-  number == number.to_i.to_s
+  integer?(number) || float?(number)
 end
 
 def get_number(turn)
   number = ''
   loop do
-    prompt "Please enter the #{turn} number:"
+    prompt MESSAGES[LANGUAGE][turn]
     number = gets.chomp
 
     break if valid_number? number
-    prompt "This doesn't look like an integer..."
+    prompt MESSAGES[LANGUAGE]['integer_warning']
   end
   number
 end
 
 def operand
-  operator_prompt = <<-MSG
-    Please choose an operation:
-    1) add,
-    2) subtract,
-    3) multiply,
-    4) divide
-  MSG
-  prompt operator_prompt
+  prompt MESSAGES[LANGUAGE]['operator_prompt']
 
   operation = ''
   loop do
     operation = gets.chomp
     break if %w(1 2 3 4).include? operation
-    prompt "Please choose 1, 2, 3, or 4."
+    prompt MESSAGES[LANGUAGE]['operator_warning']
   end
   operation
 end
 
-loop do # main loop
-  number1 = get_number "first"
+prompt MESSAGES[LANGUAGE]['welcome']
 
-  number2 = get_number "second"
+loop do # main loop
+  number1 = get_number "first_number"
+
+  number2 = get_number "second_number"
 
   operation = operand
 
@@ -53,9 +61,9 @@ loop do # main loop
            when '4' then number1.to_f / number2.to_f
            end
 
-  prompt "The result is #{result}"
+  prompt MESSAGES[LANGUAGE]['result'] + result.to_s
 
-  prompt "Do you want to perform another operation? (Y to continue)"
+  prompt(MESSAGES[LANGUAGE]['another_operation'])
   answer = gets.chomp
   break unless answer.downcase.start_with? 'y'
 end # end main loop
