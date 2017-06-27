2110a437e2e296a894adbf12b036ffbdde169f3b
srobert1953 <srobert1953@gmail.com>
2017-06-27 07:52:26 +0200

    Loan Calculator refractored

--- 101_Programming_Foundations/assignment_loan_calculator/loan_calculator.rb
+++ 101_Programming_Foundations/assignment_loan_calculator/loan_calculator.rb
@@ -23,61 +23,15 @@ end
 
 def years?(years)
   is_year = years == years.to_i.to_s
-  is_year && years.to_i <= 30 ? true : false
+  is_year && years.to_i <= 30
 end
 
-def create_number(str)
+def extract_number(str)
   number = str.split('').select { |i| i.to_i > 0 || i == '0' || i == '.' }
-  number = number.join.to_f
-  number
+  number.join.to_f
 end
 
-prompt MESSAGES[LANG]['welcome']
-prompt MESSAGES[LANG]['your_name']
-
-name = ''
-loop do
-  name = gets.chomp.capitalize
-  break if name? name
-  prompt MESSAGES[LANG]['warning_name']
-end
-
-prompt MESSAGES[LANG]['introduction']
-
-loan_amount = ''
-loop do
-  prompt MESSAGES[LANG]['loan_amount'].ljust(WIDTH - 4, '.') + ': '.rjust(2)
-  loan_amount = gets.chomp
-  break if dollars? loan_amount
-  prompt MESSAGES[LANG]['warning_amount']
-end
-
-apr = ''
-loop do
-  prompt MESSAGES[LANG]['apr'].ljust(WIDTH - 4, '.') + ': '.rjust(2)
-  apr = gets.chomp
-  break if number? apr
-  prompt MESSAGES[LANG]['warning_number']
-end
-
-loan_duration_years = ''
-loop do
-  prompt MESSAGES[LANG]['loan_duration'].ljust(WIDTH - 4, '.') + ': '.rjust(2)
-  loan_duration_years = gets.chomp
-  break if years? loan_duration_years
-  prompt MESSAGES[LANG]['warning_years']
-end
-
-loan_amount = create_number(loan_amount)
-apr = create_number(apr)
-loan_dur_years = loan_duration_years.to_i
-loan_dur_months = loan_dur_years * 12
-monthly_rate = apr / 12 / 100
-
-partial_calc = monthly_rate / (1 - (1 + monthly_rate)**(-1 * loan_dur_months))
-monthly_installment = loan_amount * partial_calc
-
-def result_message(ann, num, sym = '')
+def display_result_message(ann, num, sym = '')
   width_third = WIDTH / 3
   width_two_thirds = (WIDTH / 3) * 2
   msg_start = MESSAGES[LANG][ann].ljust(width_two_thirds, '.')
@@ -94,10 +48,55 @@ def result_message(ann, num, sym = '')
   print "\n#{msg}\n"
 end
 
+prompt MESSAGES[LANG]['welcome']
+prompt MESSAGES[LANG]['your_name']
+
+name = ''
+loop do
+  name = gets.chomp.capitalize
+  break if name? name
+  prompt MESSAGES[LANG]['warning_name']
+end
+
+prompt MESSAGES[LANG]['introduction']
+
+loan_amount = ''
+loop do
+  prompt MESSAGES[LANG]['loan_amount'].ljust(WIDTH - 3, '.') + ': '.rjust(2)
+  loan_amount = gets.chomp
+  break if dollars? loan_amount
+  prompt MESSAGES[LANG]['warning_amount']
+end
+
+apr = ''
+loop do
+  prompt MESSAGES[LANG]['apr'].ljust(WIDTH - 3, '.') + ': '.rjust(2)
+  apr = gets.chomp
+  break if number? apr
+  prompt MESSAGES[LANG]['warning_number']
+end
+
+duration_years = ''
+loop do
+  prompt MESSAGES[LANG]['loan_duration'].ljust(WIDTH - 3, '.') + ': '.rjust(2)
+  duration_years = gets.chomp
+  break if years? duration_years
+  prompt MESSAGES[LANG]['warning_years']
+end
+
+loan_amount = extract_number(loan_amount)
+apr = extract_number(apr)
+duration_years = duration_years.to_i
+duration_months = duration_years * 12
+monthly_rate = apr / 12 / 100
+
+partial_calc = monthly_rate / (1 - (1 + monthly_rate)**(-1 * duration_months))
+monthly_installment = loan_amount * partial_calc
+
 puts ""
-puts " #{name} #{MESSAGES[LANG]['result']} ".center(WIDTH, '-')
-result_message('result_amount', loan_amount, '$')
-result_message('result_apr', apr, '%')
-result_message('result_years', loan_duration_years)
-result_message('result_payment', monthly_installment, '$')
+puts " #{name}, #{MESSAGES[LANG]['result']} ".center(WIDTH, '-')
+display_result_message('result_amount', loan_amount, '$')
+display_result_message('result_apr', apr, '%')
+display_result_message('result_years', duration_years)
+display_result_message('result_payment', monthly_installment, '$')
 puts ""
