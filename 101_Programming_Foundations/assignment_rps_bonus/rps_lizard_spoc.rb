4521e1f2a72dbf05581a31d449ff93a783975faf
srobert1953 <srobert1953@gmail.com>
2017-06-29 18:22:08 +0200

    Original Message added, screan cleaning added, upcase <> downcase fixed, minor UI changes

--- 101_Programming_Foundations/assignment_rps_bonus/rps_lizard_spoc.rb
+++ 101_Programming_Foundations/assignment_rps_bonus/rps_lizard_spoc.rb
@@ -13,9 +13,20 @@ WIN_COMBINATIONS = {
   lizard: ['spock', 'paper'],
   spock: ['scissors', 'rock']
 }
+ANSWERS = {
+  "scissors" => ["cuts Paper", "decapitates Lizard"],
+  "paper" => ["covers Rock", "disproves Spock"],
+  "rock" => ["crashes Lizard", "as it always has, crashes Scissors"],
+  "lizard" => ["poisons Spock", "eats Paper"],
+  "spock" => ["smashes Scissors", "vaporises Rock"]
+}
 
 def prompt(message)
   puts "=> #{message}"
+end
+
+def clear_screen
+  system("cls") || system("clear")
 end
 
 def correct_choice?(answer)
@@ -47,6 +58,17 @@ def winner(user, computer)
   end
 end
 
+def description(user, computer, winner)
+  case winner
+  when :user
+    idx = WIN_COMBINATIONS[user.to_sym].find_index computer
+    user.capitalize + " " + ANSWERS[user][idx]
+  when :computer
+    idx = WIN_COMBINATIONS[computer.to_sym].find_index user
+    computer.capitalize + " " + ANSWERS[computer][idx]
+  end
+end
+
 def messages(winner)
   case winner
   when :user then MESSAGES[LANG]['user_win']
@@ -71,41 +93,65 @@ def grand_winner(score)
   end
 end
 
-prompt MESSAGES[LANG]['welcome']
+def display_score(card)
+  width = 42
+  puts ""
+  puts " SCORE ".center(width, '-')
+  puts  "YOU #{card[:user]}".ljust(width / 2) +
+        "#{card[:computer]} COMPUTER".rjust(width / 2)
+  puts ""
+end
+
+def game_over?(score)
+  score.values.any? { |val| val == 5 }
+end
+
+def display_wrong_choise(count)
+  if count < 1
+    prompt MESSAGES[LANG]['input_warning']
+  else
+    prompt MESSAGES[LANG]['short_input_warning']
+  end
+end
 
 loop do # main loop
   round = 0
   score_card = { user: 0, computer: 0 }
 
+  clear_screen
+
+  prompt MESSAGES[LANG]['welcome']
+
   loop do
-    puts ""
     prompt MESSAGES[LANG]['rounds'] + " #{round + 1}."
     choice = ''
+    wrong_choise_count = 0
 
-    prompt "Choose one: #{VALID_VALUES.join(', ')}"
+    prompt "#{VALID_VALUES.join(', ')}?"
     loop do
-      choice = gets.chomp
+      choice = gets.chomp.downcase
       break if correct_choice? choice
-      prompt MESSAGES[LANG]['input_warning']
+      display_wrong_choise wrong_choise_count
+      wrong_choise_count += 1
     end
 
     choice = full_choice_name(choice) unless full_name?(choice)
     computer_choice = VALID_VALUES.sample
 
     prompt "You: #{choice.upcase}, Computer: #{computer_choice.upcase}"
+
     winner = winner(choice, computer_choice)
+    prompt description(choice, computer_choice, winner)
     prompt messages(winner)
 
     round += 1
     update_score score_card, winner
-    break if score_card.values.any? { |val| val == 5 }
+
+    display_score score_card
+
+    break if game_over? score_card
   end
 
-  width = 60
-  puts ""
-  puts " SCORE ".center(width, '-')
-  puts  "YOU #{score_card[:user]}".ljust(width / 2) +
-        "#{score_card[:computer]} COMPUTER".rjust(width / 2)
   puts MESSAGES[LANG]['grand_winner'] + " #{grand_winner(score_card)}"
   puts ""
 
@@ -113,7 +159,7 @@ loop do # main loop
 
   answer = ''
   loop do
-    answer = gets.chomp
+    answer = gets.chomp.downcase
     break if correct_answer? answer
     prompt MESSAGES[LANG]['continue_warning']
   end
