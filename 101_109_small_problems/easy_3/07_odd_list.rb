6afca924d5f75634f792deac7cbce87f65ada7bc
srobert1953 <srobert1953@gmail.com>
2017-07-20 19:46:02 +0200

    removed incorrect method

--- 101_109_small_problems/easy_3/07_odd_list.rb
+++ 101_109_small_problems/easy_3/07_odd_list.rb
@@ -29,13 +29,3 @@ p oddities(['abc', 'def']) == ['abc']
 p oddities([123]) == [123]
 p oddities([]) == []
 
-def oddities(arr)
-  arr.select do |num|
-    arr.index(num).even?
-  end
-end
-
-p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
-p oddities(['abc', 'def']) == ['abc']
-p oddities([123]) == [123]
-p oddities([]) == []
