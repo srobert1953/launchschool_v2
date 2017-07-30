4b12bc509ec040090c03184de3bd82f291c77a2c
srobert1953 <srobert1953@gmail.com>
2017-07-30 16:36:10 +0200

    Diamonds finished

--- 101_109_small_problems/medium_1/05_diamonds.rb
+++ 101_109_small_problems/medium_1/05_diamonds.rb
@@ -7,11 +7,30 @@ def print_row(side, center_side)
   puts stars.center(side)
 end
 
-def diamond(side)
+def diamond(side, full = 'full')
   side_half = side / 2
   side_half.downto(0) { |idx| print_row(side, idx) }
   1.upto(side_half) { |idx| print_row(side, idx) }
 end
+
+def print_empty_row(side, center_side)
+  row = [' '] * side
+  row[center_side] = '*'
+  row[-center_side-1] = '*'
+  puts row.join
+end
+
+def empty_diamond(side)
+  side_half = side / 2
+  side_half.downto(0) { |idx| print_empty_row(side, idx) }
+  1.upto(side_half) { |idx| print_empty_row(side, idx) }
+end
+
+
+
+empty_diamond 7
+empty_diamond 9
+empty_diamond 1
 
 diamond(1)
 diamond(9)
