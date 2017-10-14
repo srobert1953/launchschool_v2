28e1e7aeefd7941c347405744a89d569383256be
srobert1953 <srobert1953@gmail.com>
2017-10-14 19:34:20 +0200

    Game v_1 completed

--- 120_Object_Oriented_Programing/lesson_5/ttt_game/ttt_game.rb
+++ 120_Object_Oriented_Programing/lesson_5/ttt_game/ttt_game.rb
@@ -1,4 +1,5 @@
 # ttt_game.rb
+require 'yaml'
 require 'pry'
 
 class Board
@@ -10,9 +11,9 @@ class Board
   attr_accessor :subsequences
   attr_reader :grid, :columns, :rows
 
-  def initialize(columns, rows)
-    @columns = columns
-    @rows = rows
+  def initialize(size)
+    @columns = size
+    @rows = size
     reset_grid
     @subsequences = 3
   end
@@ -37,7 +38,9 @@ class Board
   end
 
   def active_markers
-    marekrs = grid.select { |_, square| square.marker != Square::INITIAL_MARKER }
+    marekrs = grid.select do |_, square|
+      square.marker != Square::INITIAL_MARKER
+    end
     markers = marekrs.map { |_, square| square.marker }
     markers.uniq
   end
@@ -46,9 +49,11 @@ class Board
 
   def create_grid(x_columns, y_rows)
     grid = {}
+    counter = 1
     (0...y_rows).each do |y|
       (0...x_columns).each do |x|
-        grid[[x, y]] = Square.new
+        grid[[x, y]] = Square.new(counter)
+        counter += 1
       end
     end
     grid
@@ -93,9 +98,11 @@ end
 class Square
   INITIAL_MARKER = ' '
   attr_accessor :marker
+  attr_reader :position
 
-  def initialize
+  def initialize(position)
     @marker = INITIAL_MARKER
+    @position = position
   end
 
   def available?
@@ -124,16 +131,31 @@ end
 
 class Human < Player
   def mark(board)
-    grid_size = board.grid.size
-    display_mark_message(grid_size)
+    display_mark_message(board)
     square_position = get_mark_answer(board)
     super(board.grid[square_position])
   end
 
   private
 
-  def display_mark_message(grid_size)
-    puts "Please choose available square (1-#{grid_size}):"
+  def display_mark_message(board)
+    if name == 'You'
+      puts "Please choose available square"
+    else
+      puts "#{name}, Please choose available square"
+    end
+    puts "(#{available_squares(board)})"
+  end
+
+  def available_squares(board)
+    squares = board.available_squares
+    squares.map!(&:position)
+    join_or(squares)
+  end
+
+  def join_or(squares)
+    squares[-1] = "or #{squares.last}" if squares.size > 1
+    squares.join(', ')
   end
 
   def get_mark_answer(board)
@@ -177,8 +199,6 @@ class Computer < Player
     if middle_square(board).available?
       middle_square(board)
     elsif computer_will_win?(board, player_marker)
-      puts "Computer will win"
-      # binding.pry
       block_square(board, player_marker)
     elsif player_will_win?(board)
       block_square(board)
@@ -204,7 +224,7 @@ class Computer < Player
   end
 
   def computer_will_win?(board, player_marker)
-    computer_squares = identify_possible_win_squares(board, for_marker = player_marker)
+    computer_squares = identify_possible_win_squares(board, player_marker)
     !computer_squares.empty?
   end
 
@@ -344,11 +364,14 @@ end
 
 class TTTGame
   MARGIN = 2
+  MESSAGES = YAML.load_file('ttt_game_messages.yml')
+  LNG = 'en'
+  DEFAULT_BOARD_SIZE = 3
 
   attr_reader :board, :players
 
   def initialize
-    @players = []
+    reset_players
   end
 
   def play
@@ -363,24 +386,108 @@ class TTTGame
 
   private
 
+  def reset_players
+    @players = []
+  end
+
   def welcome_screen
     clear_screen
-    puts "Welcome to Tic Tac Toe game."
-    game_type = ask_question('Quick game type (play)', ['play', 'set'])
+    puts MESSAGES[LNG]['welcome']
+    game_type = ask_question(MESSAGES[LNG]['game_type'], ['play', 'set'])
     setup_screen(game_type)
   end
 
   def goodbye_screen
     puts ''
-    puts 'Thank you for playing Tic Tac Toe!'
+    puts MESSAGES[LNG]['good_bye']
   end
 
   def setup_screen(game_type)
     if quick_play?(game_type)
       initialize_quick_game
-    # else
-    #   setup_game
+    else
+      setup_game
     end
+  end
+
+  def setup_game
+    clear_screen
+    puts MESSAGES[LNG]['setup_welcome']
+    puts MESSAGES[LNG]['setup_prologue']
+    verison_options = ['single', 'multi']
+    game_version = ask_question(MESSAGES[LNG]['game_version'], verison_options)
+    if game_version == :single
+      setup_single_player_game
+    else
+      setup_multi_player_game
+    end
+  end
+
+  def setup_single_player_game
+    name = ask_name('ask_name')
+    puts MESSAGES[LNG]['ask_symbol']
+    symbol = ask_symbol
+    setup_single_players(name, symbol)
+    setup_board
+    start_game
+  end
+
+  def setup_multi_player_game
+    player1_name = ask_name('player_one_name')
+    player2_name = ask_name('player_two_name')
+    symbol1, symbol2 = player_symbols(player1_name, player2_name)
+    setup_multi_players(player1_name, player2_name, symbol1, symbol2)
+    setup_board
+    start_game
+  end
+
+  def player_symbols(name1, name2)
+    puts MESSAGES[LNG]['ask_symbol']
+    puts "#{name1} symbol:"
+    symbol_player1 = ask_symbol
+    if symbol_player1.empty?
+      symbol_player2 = 'O'
+    else
+      puts "#{name2} symbol:"
+      symbol_player2 = ask_symbol
+    end
+    [symbol_player1, symbol_player2]
+  end
+
+  def setup_single_players(name, symbol)
+    symbol = symbol.empty? ? 'X' : symbol
+    reset_players
+    players << Human.new(name, symbol)
+    players << Computer.new('Computer', 'O')
+  end
+
+  def setup_multi_players(name1, name2, symbol1, symbol2)
+    symbol1 = symbol1.empty? ? 'X' : symbol1
+    reset_players
+    players << Human.new(name1, symbol1)
+    players << Human.new(name2, symbol2)
+  end
+
+  def setup_board
+    board_sizes = ['3x3', '4x4', '5x5', '6x6', '7x7', '8x8', '9x9']
+    size = ask_question(MESSAGES[LNG]['board_size'], board_sizes)
+    subsequences = if size == :'3x3'
+                     3
+                   else
+                     ask_subsequences(size)
+                   end
+    initialize_board(size, subsequences)
+  end
+
+  def initialize_board(size, subsequences)
+    size = size.to_s[0].to_i
+    @board = Board.new(size)
+    board.subsequences = subsequences
+  end
+
+  def start_game
+    puts MESSAGES[LNG]['start_new_game']
+    gets
   end
 
   def quick_play?(game_type)
@@ -396,14 +503,18 @@ class TTTGame
   def players_move
     no_of_players = players.size
     loop do
-      game_screen
       (0...no_of_players).each do |idx|
+        display_game_screen
         current_player = players[idx]
         current_player.mark(board)
         current_player.update_score if board.winner?(current_player)
         return if board.winner?(current_player) || board.full?
       end
     end
+  end
+
+  def display_game_screen
+    board.grid.size > 9 ? game_screen(true) : game_screen
   end
 
   def display_result
@@ -423,16 +534,57 @@ class TTTGame
     nil
   end
 
+  def ask_subsequences(size)
+    size_integer = size.to_s[0].to_i
+    subsequences = nil
+    loop do
+      puts MESSAGES[LNG]['ask_subsequences']
+      prompt
+      subsequences = gets.chomp.to_i
+      break if subsequences <= size_integer && subsequences >= 3
+      puts MESSAGES[LNG]['incorrect_subsequences']
+    end
+    subsequences
+  end
+
+  def ask_symbol
+    symbol = nil
+    loop do
+      prompt
+      symbol = gets.chomp
+      break if symbol.size <= 1
+      puts MESSAGES[LNG]['incorrect_symbol']
+    end
+    symbol
+  end
+
+  def ask_name(game_type)
+    name = nil
+    loop do
+      puts MESSAGES[LNG][game_type]
+      prompt
+      name = gets.chomp.capitalize
+      break unless name.empty?
+      puts MESSAGES[LNG]['empty_response']
+    end
+    name
+  end
+
   def ask_question(question, answers)
     user_answer = nil
     puts question
     loop do
-      print "=> "
+      prompt
       user_answer = gets.chomp.downcase
       break if answers.any? { |choice| choice.start_with? user_answer }
       puts "Please choose one of the following: #{answers.join(', ')}"
     end
     convert_to_symbol(user_answer, answers)
+  end
+
+  def prompt
+    puts ''
+    print "=> "
   end
 
   def convert_to_symbol(answer, choises)
@@ -441,19 +593,20 @@ class TTTGame
   end
 
   def initialize_quick_game
-    @board = Board.new(3, 3)
+    @board = Board.new(DEFAULT_BOARD_SIZE)
     configure_quick_game_players
   end
 
   def configure_quick_game_players
-    players.push Human.new('Robert', 'X')
+    reset_players
+    players.push Human.new('You', 'X')
     players.push Computer.new('Computer', 'O')
   end
 
-  def game_screen
+  def game_screen(help = false)
     clear_screen
     display_score
-    display_grid
+    display_grid(help)
   end
 
   def clear_screen
@@ -487,11 +640,11 @@ class TTTGame
       ": #{player.score}".rjust(score_size)
   end
 
-  def display_grid
+  def display_grid(help)
     puts ''
     board.rows.times do |row|
       board_row_separator(row, board.columns)
-      print_squares(board.grid, row)
+      print_squares(board.grid, row, help)
     end
     puts ''
   end
@@ -500,34 +653,39 @@ class TTTGame
     puts ' ' * MARGIN + '-----+' * (columns - 1) + '-----' unless row.zero?
   end
 
-  def print_squares(grid, row)
+  def print_squares(grid, row, help)
     row = grid.select { |position, _| position.last == row }
     3.times do |idx|
-      square_line(row, idx)
+      square_line(row, idx, help)
     end
   end
 
-  def square_line(row, line_idx)
+  def square_line(row, line_idx, help)
     line = row.map do |position, square|
-      box_fill(position, square.marker)[line_idx]
+      box_fill(position, square, help)[line_idx]
     end
     puts line.join
   end
 
-  def box_fill(position, marker)
+  def box_fill(position, square, help)
+    help_number = help ? square.position : ' '
     if position.first.zero?
       [
-        ' ' * MARGIN + '     ',
-        ' ' * MARGIN + "  #{marker}  ",
+        ' ' * MARGIN + helping_number(help_number),
+        ' ' * MARGIN + "  #{square.marker}  ",
         ' ' * MARGIN + '     '
       ]
     else
       [
-        '|     ',
-        "|  #{marker}  ",
+        "|" + helping_number(help_number),
+        "|  #{square.marker}  ",
         '|     '
       ]
     end
+  end
+
+  def helping_number(help_number)
+    " " * (5 - help_number.to_s.size) + help_number.to_s
   end
 end
 
