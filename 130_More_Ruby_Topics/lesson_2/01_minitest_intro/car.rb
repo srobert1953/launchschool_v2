311735b139923dad232d00e7aafdb78379647732
srobert1953 <srobert1953@gmail.com>
2017-11-15 08:48:29 +0100

    Minitest introduction

---
+++ 130_More_Ruby_Topics/lesson_2/01_minitest_intro/car.rb
@@ -0,0 +1,9 @@
+# car.rb
+
+class Car
+  attr_accessor :wheels
+
+  def initialize
+    @wheels = 4
+  end
+end
---
+++ 130_More_Ruby_Topics/lesson_2/01_minitest_intro/car_test.rb
@@ -0,0 +1,20 @@
+# car_test.rb
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+MiniTest::Reporters.use!
+
+require_relative 'car'
+
+class CarTest < MiniTest::Test
+  def test_wheels
+    car = Car.new
+    assert_equal(4, car.wheels)
+  end
+
+  def test_bad_wheels
+    skip("Will be tested later")
+    car = Car.new
+    assert_equal(3, car.wheels)
+  end
+end
