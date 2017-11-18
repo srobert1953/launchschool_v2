df953172eae24afe68ce26cc867f1d5d877d0832
srobert1953 <srobert1953@gmail.com>
2017-11-18 16:45:28 +0100

    Small changes

--- 130_139_more_ruby_foundation/easy_1/10_count_items.rb
+++ 130_139_more_ruby_foundation/easy_1/10_count_items.rb
@@ -1,11 +1,11 @@
 class MyClass
   def count(collection)
+    # => Solution 1
     # counter = 0
-
     # collection.each { |item| counter += 1 if yield(item) }
-
     # counter
 
+    # => Solution 2
     collection.reduce(0) do |result, item|
       result += 1 if yield(item)
       result
