83c9e8b3d6730ed7d12d4fa32dd64e7c0d11fac2
srobert1953 <srobert1953@gmail.com>
2017-11-18 10:47:43 +0100

    Added different solution

--- 130_139_more_ruby_foundation/easy_1/10_count_items.rb
+++ 130_139_more_ruby_foundation/easy_1/10_count_items.rb
@@ -1,9 +1,14 @@
 class MyClass
   def count(collection)
-    counter = 0
+    # counter = 0
 
-    collection.each { |item| counter += 1 if yield(item) }
+    # collection.each { |item| counter += 1 if yield(item) }
 
-    counter
+    # counter
+
+    collection.reduce(0) do |result, item|
+      result += 1 if yield(item)
+      result
+    end
   end
 end
